a327dbf6d4725d7dbbad656d9819c7a1d5e9d1dc
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2025-07-03 06:16:38 -0600

    Sticking with the same number of elements as vanilla benchmarks

--- src/benchmarks/rma/shmem_sec_put.c
+++ src/benchmarks/rma/shmem_sec_put.c
@@ -58,7 +58,7 @@ void bench_shmem_sec_put_bw(int min_msg_size, int max_msg_size, int ntimes) {
     for (int j = 0; j < ntimes; j++) {
 #if defined(USE_14) || defined(USE_15)
        if (!shmem_my_pe())
-          shmemx_secure_put(SHMEM_CTX_DEFAULT, dest, source, elem_count * sizeof(double), 1);
+          shmemx_secure_put(SHMEM_CTX_DEFAULT, dest, source, elem_count, 1);
        shmem_barrier_all();
        //shmem_put(dest, source, elem_count, 1);
 #endif
@@ -77,8 +77,8 @@ void bench_shmem_sec_put_bw(int min_msg_size, int max_msg_size, int ntimes) {
     bandwidths[i] = calculate_bw(valid_size, times[i]);
 
     /* Free the buffers */
-    shmem_free(source);
-    shmem_free(dest);
+    //shmem_free(source);
+    //shmem_free(dest);
   }
 
   /* Display results */

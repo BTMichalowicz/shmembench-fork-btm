1fc9a6522d0fc3684bb246a9e04a2af9392a73c7
Graysen Gould <gouldgraysen@gmail.com>
2026-01-30 10:08:23 -0600

    Fixed typo

--- src/benchmarks/atomics/shmem_atomic_swap.c
+++ src/benchmarks/atomics/shmem_atomic_swap.c
@@ -8,7 +8,7 @@
 
 /**
   @brief Run the latency benchmark for shmem_atomic_swap
-  @param opts Benchmarks options given by the user 
+  @param opts Benchmark options given by the user 
  */
 void bench_shmem_atomic_swap_latency(options * opts) {
   /* Check if there are enough PEs to run the benchmark */
--- src/benchmarks/rma/shmem_get_nbi.c
+++ src/benchmarks/rma/shmem_get_nbi.c
@@ -7,7 +7,7 @@
 
 /**
   @brief Run the bandwidth benchmark for shmem_get_nbi
-  @param opts Benchmarks options given by the user 
+  @param opts Benchmark options given by the user 
  */
 void bench_shmem_get_nbi_bw(options * opts) {
   /* Check the number of PEs before doing anything */
@@ -106,7 +106,7 @@ void bench_shmem_get_nbi_bw(options * opts) {
 
 /**
   @brief Run the bidirectional bandwidth benchmark for shmem_get_nbi
-  @param opts Benchmarks options given by the user 
+  @param opts Benchmark options given by the user 
  */
 void bench_shmem_get_nbi_bibw(options * opts) {
   /* Check the number of PEs before doing anything */
@@ -203,7 +203,7 @@ void bench_shmem_get_nbi_bibw(options * opts) {
 
 /*************************************************************
   @brief Run the latency benchmark for shmem_get_nbi
-  @param opts Benchmarks options given by the user 
+  @param opts Benchmark options given by the user 
  *************************************************************/
 void bench_shmem_get_nbi_latency(options * opts) {
   /* Check the number of PEs before doing anything */

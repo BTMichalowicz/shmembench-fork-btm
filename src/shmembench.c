4940e7d1d9395e9d27b93fb9865a68dc42dc9014
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2026-05-10 14:13:20 -0400

    Set up a basic operation for the benchmarks. I can also add in the option in the command line arguments

--- src/shmembench.c
+++ src/shmembench.c
@@ -58,7 +58,10 @@ benchmark_entry_t benchmark_table[] = {
     {"shmem_getmem_nbi", "bw", bench_shmem_getmem_nbi_bw, NULL, NULL, false},
     {"shmem_getmem_nbi", "bibw", bench_shmem_getmem_nbi_bibw, NULL, NULL, false}, 
 
-    {"shmem_alltoall", "bw", bench_shmem_alltoall_bw, NULL, NULL, false}, 
+
+    {"shmem_reduce", "bw", bench_shmem_reduce_bw, NULL, NULL, false},
+
+    {"shmem_alltoall", "bw", bench_shmem_alltoall_bw, NULL, NULL, false},
     {"shmem_alltoallmem", "bw", bench_shmem_alltoallmem_bw, NULL, NULL, false},
 
     {"shmem_alltoalls", "bw", bench_shmem_alltoalls_bw, NULL, NULL, false},
